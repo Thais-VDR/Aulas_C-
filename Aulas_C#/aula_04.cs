ec3949318445db9fc52d3f6daa5aed7d0075a385
THAÍS VITÓRIA <127230165+Thais-VDR@users.noreply.github.com>
2025-10-07 08:40:21 -0400

    Add files via upload

---
+++ Aulas_C#/aula_04.cs
@@ -0,0 +1,15 @@
+﻿using aula04;
+
+int iniciarPrograma = 1;
+
+Controller _controller = new Controller();
+Console.WriteLine(" Programa Iniciado! ");
+_controller.MostrarMenu();
+while (iniciarPrograma == 1)
+{
+    
+   
+    int opcao = int.Parse(Console.ReadLine()!);
+    iniciarPrograma = _controller.ProcessarOpcao(opcao);
+    
+}
